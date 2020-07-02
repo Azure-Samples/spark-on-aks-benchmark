8436847df1ebfb89f2e563d28e3b055ec3bc0e7a
Jonathan Gardner (MSFT) <jgardner04@outlook.com>
2020-07-02 16:35:15 -0700

    [Breaking Change] - Remove aad managed cluster

--- env/modules/aks/aks.tf
+++ env/modules/aks/aks.tf
@@ -2,25 +2,22 @@
 # Licensed under the MIT license
 
 resource "azurerm_kubernetes_cluster" "aks" {
-  name = "${var.name}-k8s"
-  location = var.location
+  name                = "${var.name}-k8s"
+  location            = var.location
   resource_group_name = var.resource_group_name
-  dns_prefix = "${var.name}-k8s"
+  dns_prefix          = "${var.name}-k8s"
 
   enable_pod_security_policy = true
   role_based_access_control {
     enabled = true
-    azure_active_directory {
-      managed = true
-    }
   }
 
   sku_tier = "Paid"
 
   default_node_pool {
-    name = "default"
-    node_count = 3
-    vm_size = var.vm_size
+    name           = "default"
+    node_count     = 3
+    vm_size        = var.vm_size
     vnet_subnet_id = var.vnet_subnet_id
   }
 
@@ -34,7 +31,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   addon_profile {
     oms_agent {
-      enabled = true
+      enabled                    = true
       log_analytics_workspace_id = var.log_analytics_id
     }
   }
