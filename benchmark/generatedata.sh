8d4a5744ceee0662413613badfadd8026f42f19a
yashkv1 <30442765+yashkv1@users.noreply.github.com>
2020-08-25 16:20:23 +0000

    Updating benchmark code (#54)
    * Cleanup
    
    * Adding benchmark scripts
    
    * Updating benchmark code

--- benchmark/generatedata.sh
+++ benchmark/generatedata.sh
@@ -18,13 +18,15 @@ echo $JARS
 echo $BENCHMARK
 
 read -p "Enter Shared Key : " $key
-
-
 ./bin/spark-submit \
         --master k8s://https://default-sparkonaks-k8s-79607165.hcp.westus2.azmk8s.io:443 \
         --deploy-mode cluster \
         --class com.microsoftazure.aks.tpcds.DataGenerator \
         --conf spark.executor.instances=10\
+        --conf spark.driver.cores=2 \
+        --conf spark.executor.cores=2 \
+        --conf spark.executor.memory=8000m \
+        --conf spark.driver.memory=8000m \
         --conf spark.app.name=tpcds \
         --conf spark.kubernetes.container.image=sparkacrbacf.azurecr.io/spark:prod1 \
         --conf spark.kubernetes.authenticate.driver.serviceAccountName=spark \
@@ -33,6 +35,7 @@ read -p "Enter Shared Key : " $key
         --conf spark.kubernetes.container.image.pullPolicy=Always \
         --conf spark.kubernetes.file.upload.path=/opt/spark/work-dir \
         --conf spark.hadoop.fs.azure.account.auth.type.sparkonakstpcdsdataset.dfs.core.windows.net=SharedKey \
+        --conf spark.hadoop.fs.azure.account.key.sparkonakstpcdsdataset.dfs.core.windows.net=$key \
         --conf spark.hadoop.fs.azure.account.key.sparkonakstpcdsdataset.dfs.core.windows.net=cqeElafHqmbkG7nymkHQLIfvGKglGSBzZOaal06n25IGGttYw1JJdl9Y58jr0Xs47TGAUXhafdP6+hVQ1nLj3w== \
          "local:///opt/spark/jars/tpcdsbenmark_2.12-0.1.0-SNAPSHOT.jar" "abfss://tpcds@sparkonakstpcdsdataset.dfs.core.windows.net/data/data" "/opt/tpcds-kit/tools" "parquet" "1000"
 
