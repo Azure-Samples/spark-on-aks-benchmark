757520bd9677632252793f291a7700ba532ce320
neeginotra <neginotr@microsoft.com>
2020-10-01 22:07:45 +0000

    changing the name for loga file

--- env/modules/log-analytics/log-analytics.tf
+++ env/modules/log-analytics/log-analytics.tf
@@ -2,7 +2,7 @@
 # Licensed under the MIT license
 
 resource "azurerm_log_analytics_workspace" "la" {
-  name = "${var.name}-la"
+  name = "${var.name}-loga"
   location = var.location
   resource_group_name = var.resource_group_name
   sku = var.sku
