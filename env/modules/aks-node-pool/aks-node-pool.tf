7b188e0d6c314807337d1893eac096924777fe86
Jonathan Gardner <jogardn@microsoft.com>
2020-10-13 10:20:51 -0700

    Update node pool OS disk size

--- env/modules/aks-node-pool/aks-node-pool.tf
+++ env/modules/aks-node-pool/aks-node-pool.tf
@@ -6,7 +6,7 @@ resource "azurerm_kubernetes_cluster_node_pool" "pool" {
   kubernetes_cluster_id = var.aks_cluster_id
   vm_size               = var.vm_size
   node_count            = var.node_count
-  os_disk_size_gb       = 1000
+  os_disk_size_gb       = 256
   vnet_subnet_id        = var.vnet_subnet_id
   mode                  = "User"
   node_labels           = var.node_labels
