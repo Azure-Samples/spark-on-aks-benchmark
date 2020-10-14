882c08272958b6cde2ebc1419a9e5321f15a29c9
Jonathan Gardner <jogardn@microsoft.com>
2020-10-14 16:00:28 -0700

    Change OS disk size to 512GB
    * Move the OS disk size to a variable in the module
    * Updated the base deployment to include the new variable
    * Changed the local for the OS disk size to be 512GB
    
    closes #106

--- env/base-cluster/aks.tf
+++ env/base-cluster/aks.tf
@@ -20,6 +20,7 @@ module "spark_node_pool" {
   name           = "spark"
   aks_cluster_id = module.aks.id
   vm_size        = local.spark_cluster_vm_size
+  os_disk_size   = 512
   node_count     = local.spark_aks_pool_size
   vnet_subnet_id = module.aks_subnet.id
   node_labels = {
