10c8420231d580d1f908b1a3919fa316778f69c9
Jonathan Gardner <jgardner04@outlook.com>
2020-12-11 17:08:24 -0800

    Add local terraform files

--- env/base-cluster/variables.tf
+++ env/base-cluster/variables.tf
@@ -1,16 +1,6 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license
 
-# Provider block variables set as GitHub secrets at action time
-variable "sub" {
-  type = string
-}
-variable "client_id" {
-  type = string
-}
-variable "client_secret" {
-  type = string
-}
-variable "tenant_id" {
+variable "workspace" {
   type = string
 }
